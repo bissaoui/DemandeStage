be1aa3851e901cec6d4a3ea364951d94101759ec
Moussatef <otman.moussetaf@gmail.com>
2021-08-31 19:31:10 +0100

    modification in

--- resources/views/dashboards/admins/Projet/details.blade.php
+++ resources/views/dashboards/admins/Projet/details.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.admin')
 
-@section('content')   
+@section('content')
 
-<div class="container-fluid">            
+<div class="container-fluid">
 
     <div class="row clearfix">
         <div class="col-lg-12">
@@ -11,9 +11,21 @@
                     <h2><strong>  Modifier </strong> Ecoles </h2>
                 </div>
                 <div class="body">
-                   
+
+
                 </div>
+
             </div>
+        </div>
+    </div>
+
+</div>
+<div class="col-lg-3 col-md-6 col-sm-12 text-center">
+    <div class="card tasks_report">
+        <div class="body">
+            <input type="text" class="knob" value="66" data-width="90" data-height="90" data-thickness="0.1" data-fgColor="#26dad2" readonly>
+            <h6 class="m-t-20">Satisfaction Rate</h6>
+            <p class="displayblock m-b-0">47% Average <i class="fa fa-trending-up"></i></p>
         </div>
     </div>
 </div>
